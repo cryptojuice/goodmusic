5e3f763d83661e03fa24fa77e19bc92a0e2d73c8
Nick Robinson <grobins2@gmail.com>
2013-11-05 11:15:15 -0500

    renamed var

--- app/search/views.py
+++ app/search/views.py
@@ -1,11 +1,15 @@
 from flask import Blueprint, request, render_template, flash, g, session, \
     redirect, url_for, jsonify
+from app.search.helpers.lastfm import LastfmSearch
+import json
 
 mod = Blueprint('search', __name__, url_prefix="/1.0/search")
 
 @mod.route("/artist=<string:artist>")
 def search_by_artist(artist):
-    return "Search Api v1.0 for " + artist
+    q = LastfmSearch(limit=5)
+    data = q.artist(artist)
+    return jsonify(data)
 
 @mod.route("/track=<string:track>")
 def search_by_track(track):
