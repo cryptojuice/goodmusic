06bbf79de8afe902bddd5b7f728031319f54e02f
Nick Robinson <grobins2@gmail.com>
2013-11-05 22:11:43 -0500

    switched from urllib to requests

--- app/search/helpers/lastfm.py
+++ app/search/helpers/lastfm.py
@@ -1,23 +1,22 @@
 from app.config.keys import LASTFM_API_KEY, LASTFM_API_SECRET
-import urllib
-import json
+import json, requests
 
 class LastfmSearch:
-    def __init__(self, limit = 30):
-        self.BASE_URI = "http://ws.audioscrobbler.com/2.0/?method="
+    def __init__(self, limit = 1):
+        self.BASE_URI = "http://ws.audioscrobbler.com/2.0/?"
         self.api_key = LASTFM_API_KEY
         self.api_secret = LASTFM_API_SECRET
         self.limit = limit
 
     def artist(self, artist):
-        results = json.loads(urllib.urlopen(self.BASE_URI + \
-                "artist.search&artist=%s&api_key=%s&limit=%s&format=json" \
-                % (artist, self.api_key, self.limit)).read())
-        return results
+        payload = {'artist':artist, 'method':'artist.search', 'format':'json', \
+                'limit':self.limit, 'api_key':self.api_key}
+        r = requests.get(self.BASE_URI, params=payload)
+        return r.json()
 
 
-    def artist(self, track):
-        results = json.loads(urllib.urlopen(self.BASE_URI + \
-                "track.search&track=%s&api_key=%s&limit=%s&format=json" \
-                % (track, self.api_key, self.limit)).read())
-        return results
+    def track(self, track):
+        payload = {'track': track, 'method':'track.search', 'format':'json', \
+                'limit':self.limit, 'api_key':self.api_key}
+        r = requests.get(self.BASE_URI, params=payload)
+        return r.json()
