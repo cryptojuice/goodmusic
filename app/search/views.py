11991df440db9ca31e3304bdabdaf32c0b65c35f
Nick Robinson <grobins2@gmail.com>
2013-11-05 22:12:32 -0500

    added limit param to artist and track search route

--- app/search/views.py
+++ app/search/views.py
@@ -5,15 +5,19 @@ import json
 
 mod = Blueprint('search', __name__, url_prefix="/1.0/search")
 
-@mod.route("/artist=<string:artist>")
-def search_by_artist(artist):
-    q = LastfmSearch(limit=5)
+@mod.route("/artist=<string:artist>", defaults={"lim":30}, methods=['GET'])
+@mod.route("/artist=<string:artist>&limit=<int:lim>", methods=['GET'])
+def search_by_artist(artist, lim):
+    q = LastfmSearch(limit=lim)
     data = q.artist(artist)
     return jsonify(data)
 
-@mod.route("/track=<string:track>")
-def search_by_track(track):
-    return "Track: %s" % track
+@mod.route("/track=<string:track>", defaults={"lim":30}, methods=['GET'])
+@mod.route("/track=<string:track>&limit=<int:lim>", methods=['GET'])
+def search_by_track(track, lim):
+    q = LastfmSearch(limit=lim)
+    data = q.track(track)
+    return jsonify(track)
 
 @mod.route("/artist=<string:artist>&track=<string:track>")
 @mod.route("/track=<string:track>&artist=<string:artist>")
