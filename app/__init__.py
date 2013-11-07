eb1f426c54354f05826d576e5bccef42f891b0a4
Nick Robinson <grobins2@gmail.com>
2013-11-07 05:22:31 -0500

    blueprint for users module

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def hello():
-    return "Hello from flask."
-
 # app blueprints
 from app.search.views import mod as searchModule
 app.register_blueprint(searchModule)
+
+from app.users.views ipmort mod as usersModule
+app.register_blueprint(usersModule)
