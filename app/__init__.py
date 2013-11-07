18c0e5864196f22c358c2d736598e4e40d166b02
Nick Robinson <grobins2@gmail.com>
2013-11-07 11:08:34 -0500

    login management with flask-login exention

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 from flask import Flask
+from flask.ext.login import LoginManager
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+# Manage Logins using flask-login extension
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # app blueprints
 from app.search.views import mod as searchModule
 app.register_blueprint(searchModule)
 
-from app.users.views ipmort mod as usersModule
+from app.users.views import mod as usersModule
 app.register_blueprint(usersModule)
--- app/users/views.py
+++ app/users/views.py
@@ -2,3 +2,17 @@ from flask import Blueprint, request, render_template, flash, g, session,\
         redirect, url_for, jsonify
 
 mod = Blueprint('users',__name__, url_prefix('/users'))
+
+
+@mod.route('/register'methods=['GET','POST'])
+def register:
+    pass
+
+@mod.route('/login', methods=['POST'])
+def login:
+    pass
+
+@mod.route('/logout', method=['POST'])
+def logout:
+    pass
+
