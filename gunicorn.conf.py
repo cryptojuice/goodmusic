0a60812a671a0f33f017ee2cfe02409c5890abfb
Nick Robinson <grobins2@gmail.com>
2013-11-08 06:06:45 -0500

    debug mode set to false

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,9 @@
 import multiprocessing
 
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 3
 bind = '127.0.0.1:8000'
 proc_name = 'gunicorn'
 pidfile = '/tmp/gunicorn.pid'
 logfile = '/tmp/gunicorn.log'
 worker_class = 'gevent'
-debug = True
+debug = False
