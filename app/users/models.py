294d6d8448848e62a37655aa5882c30665581b9f
Nick Robinson <grobins2@gmail.com>
2013-11-08 12:38:35 -0500

    flask-login authentication added

--- app/users/models.py
+++ app/users/models.py
@@ -1,4 +1,21 @@
 from app.config.database import db
 
-
 collection = db.users
+
+class User:
+
+    def __init__(self, _id=None, email=None, username=None, password=None):
+        self._id = _id
+        self.email = email
+        self.username = username
+        self.password = password
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+    
