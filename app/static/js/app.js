33d7820af82be8201f3141090e69d45d4cd83b1d
Nick Robinson <grobins2@gmail.com>
2013-11-19 13:00:40 -0500

    switched from angular-router to angular-ui-router

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,16 +1,30 @@
 angular.module('goodmusic', [
+        'ui.router',
         'ngRoute', 
         'ngStorage', 
         'ngResource',
+        'NavModule',
         'LoginModule',
         'SearchModule'
         ]);
 
-angular.module('goodmusic').config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-    .when('/login', { templateUrl: 'partials/login.html', controller: 'LoginController'})
-    .when('/search', { templateUrl: 'partials/search.html', controller: 'SearchController'})
-    .otherwise({ redirectTo: '/' });
+angular.module('goodmusic').config(['$routeProvider', '$stateProvider', '$locationProvider', 
+        function($routeProvider, $stateProvider, $locationProvider) {
+
+    $stateProvider.state('home', {
+        url: '/',
+        template:"<h1>you are home</h1>"
+    });
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'partials/login.html',
+        controller: 'LoginController'
+    });
+    $stateProvider.state('search', {
+        url: '/search',
+        templateUrl: 'partials/search.html',
+        controller: 'SearchController'
+    });
 
     $locationProvider.html5Mode(true);
 }]);
