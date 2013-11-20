79c50d7b0e2c5242fdbdcdaf71efea120658211e
Nick Robinson <grobins2@gmail.com>
2013-11-20 10:22:54 -0500

    type in artist search route

--- app/search/views.py
+++ app/search/views.py
@@ -4,7 +4,7 @@ import app.search.helpers.lastfm as lastfm
 mod = Blueprint('search', __name__, url_prefix="/api/v1/search")
 
 @mod.route("/artist=<string:artist>", defaults={"lim":30}, methods=['GET'])
-@mod.route("artist=<string:artist>&limit=<int:lim>", methods=['GET'])
+@mod.route("/artist=<string:artist>&limit=<int:lim>", methods=['GET'])
 def search_by_artist(artist, lim):
     q = lastfm.LastfmSearch(limit=lim)
     data = q.artist(artist)
